708abd32aa530fe8525ec24321e1b00f7ac5ca81
Don <55897579+DonDanon@users.noreply.github.com>
2020-06-27 21:58:34 +0300

    Добавлены Варны.
    Варны добавлены в модуль Moderation.
    Если не будет дел сделаю завтра мьют, хотя врятли дел не будет, деревня же.

--- Modules/moderation.py
+++ Modules/moderation.py
@@ -18,6 +18,69 @@ class moderation(commands.Cog):
         self.bot = bot
         self._last_member = None
 
+    @commands.command(
+       aliases=["пред", "варн"],
+       description="Выдать предупреждение пользователю")
+   @commands.has_permissions(kick_members=True)
+   async def warn(self, ctx, member: discord.Member = None, *, reason=None):
+       conn = pymongo.MongoClient(f"mongodb+srv://{config.db_user}:{config.db_password}@rinoliku-2-qvq4c.mongodb.net/{config.db_name}?retryWrites=true&w=majority")
+       db = conn[f"RB_DB"]
+       cursor = db[f"members_warns"]
+       ids = 0
+
+       for i in cursor.find({"guild": f"{message.guild.id}"}).sort("id", -1):
+           if not i["id"]:
+               ids = 1
+               break
+           else:
+               ids = i["id"] + 1
+               break
+
+       cursor.insert_one(
+           {"id": ids, "guild": f"{message.guild.id}", "member": f"{member.id}", "moderator": f"{moderator}","reason": f"{reason}"})
+
+       await ctx.send(f"Предупреждение пользователю {member.display_name} с причиной {reason} успешно выдано! (ID предупреждения - `{number}`)")
+
+   @commands.command(
+       aliases=["убратьпред", "убратьварн"],
+       description="Забрать предупреждение у пользовател через ID предупреждения")
+   @commands.has_permissions(kick_members=True)
+   async def unwarn(self, ctx, ids: int):
+       conn = pymongo.MongoClient(f"mongodb+srv://{config.db_user}:{config.db_password}@rinoliku-2-qvq4c.mongodb.net/{config.db_name}?retryWrites=true&w=majority")
+       db = conn[f"RB_DB"]
+       cursor = db[f"members_warns"]
+
+       cursor.delete_one({"guild": f"{message.guild.id}", "id": ids})
+       await ctx.send(f"Предупреждение пользователю {member.display_name} успешно убрано!")
+
+   @commands.command(
+       aliases=["преды", "варны"],
+       description="Посмотреть предупреждения пользователя")
+   @commands.has_permissions(kick_members=True)
+   async def warns(self, ctx, member: discord.Member = None):
+       if not member:
+           member = ctx.message.author
+
+       warn_list = []
+
+       conn = pymongo.MongoClient(f"mongodb+srv://{config.db_user}:{config.db_password}@rinoliku-2-qvq4c.mongodb.net/{config.db_name}?retryWrites=true&w=majority")
+       db = conn[f"RB_DB"]
+       cursor = db[f"members_warns"]
+
+       cursor.find({"guild": f"{message.guild.id}", "user": f"{member.id}"}).sort("id", -1)
+
+       for i in warns:
+           moder = ctx.guild.get_member(int(i['moderator']))
+           warn_list.append(f"`ID - {i['id']}` | Выдал - {moder.display_name} | Причина - {i['reason']}\n")
+
+       if not warn_list:
+           warn_list = ["Пользователь не имеет предупреждений"]
+
+       embed = discord.Embed(title=f"Предупреждения | {member.display_name}", description="".join(warn_list))
+       embed.set_footer(text=config.copy, icon_url=config.icon)
+
+       await ctx.send(embed=embed)
+
 # <!-- Бан -->
     @commands.command(aliases=["Бан", "бан", "Ban"])
     @commands.has_permissions(ban_members=True)
