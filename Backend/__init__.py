7b112dc2b75e4746db5296a7e1072a05a97b3d46
Егор Кислый <kislball@yahoo.com>
2020-07-03 22:48:41 +0300

    рефактор бекенда

--- Backend/__init__.py
+++ Backend/__init__.py
@@ -3,34 +3,13 @@ from discord.ext import commands
 import config
 import threading
 import json
-
+from Backend.public import initRoutes
 class RinokuBackend(commands.Cog):
   def __init__(self, bot):
     self.bot = bot
     self.app = Flask(__name__)
 
-    
-
-    @self.app.route("/public/fetch_commands")
-    def main():
-      result = []
-
-      for command in self.bot.commands:
-        if command.hidden:
-          continue
-        result.append({
-          'name': command.name,
-          'description': command.description
-        })
-
-      resp = Response(json.dumps({
-        "commands": result
-      }))
-
-      resp.headers['content-type'] = "text/plain; charset=utf-8"
-
-      return resp
-
+    initRoutes(self.bot, self.app)
 
     thread1 = threading.Thread(target= self.app.run)
     thread1.start()
---
+++ Backend/public.py
@@ -0,0 +1,21 @@
+import json
+from flask import Response
+
+def initRoutes(bot, app):
+  @app.route("/public/fetch_commands", endpoint="fetch_commands")
+  def fetch_commands():
+      result = []
+
+      for command in bot.commands:
+          if command.hidden:
+            continue
+          result.append({
+              'name': command.name,
+              'description': command.description
+          })
+
+      resp = Response(json.dumps({"commands": result}))
+
+      resp.headers['content-type'] = "text/plain; charset=utf-8"
+
+      return resp
