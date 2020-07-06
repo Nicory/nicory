60a1e3d099bae1996ad2c446ad00f0895e4a8796
Егор Кислый <kislball@yahoo.com>
2020-07-06 21:21:25 +0300

    /private/info endpoint

--- Backend/private.py
+++ Backend/private.py
@@ -1,12 +1,14 @@
 import json
-from flask import make_response
+from flask import make_response, request, Response
 import config
 from pymongo import MongoClient
-
+from Backend.utils import auth
+from discord import Status
+from flask_api import status as http_status
 
 def initRoutes(bot, app):
   @app.route("/private/auth/<token>", endpoint="auth")
-  def auth(token):
+  def auth_token(token):
     conn = MongoClient(config.MONGODB)
     db = conn["RB_DB"]
     cursor = db['admin_tokens']
@@ -30,13 +32,32 @@ def initRoutes(bot, app):
           'error_code': 2
       }, 401)
 
-    print(type(str(result['guild'])))
     return make_response({
-      guild: int(result['guild']),
-      member: int(result['member'])
+      'guild': int(result['guild']),
+      'member': int(result['member'])
     }, 200)
 
+  @app.route("/private/info")
+  def guild_info():
+    if not 'Authorization' in request.headers:
+      return Response(status=401)
     
+    token = auth(request.headers["Authorization"], bot)
+    if 'error_code' in token:
+      return Response(status=401)
+
+    gld = bot.get_guild(token['guild'])
+
+    def fil(el):
+      print(el.status)
+      return el.status != Status.offline
+
+    return {
+      'name': str(gld),
+      'total_members': len(gld.members),
+      'online_members': len(list(filter(fil, gld.members))),
+      'icon': str(gld.icon_url)
+    }
 
     
 
