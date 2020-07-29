9270615ddf948a947238f5946206a60164b11c49
Егор Кислый <kislball@yahoo.com>
2020-07-29 22:33:34 +0300

    split bot's loading in different files

--- index.js
+++ index.js
@@ -1,45 +1,17 @@
 const fs = require('fs');
 const Discord = require('discord.js');
 const { token } = require('./config.json');
-const isAsync = (f) => f.constructor.name === 'AsyncFunction';
-const chalk = require('chalk');
 const processCommand = require('./utils/processCommand');
 
 console.log(fs.readFileSync('./assets/banner.txt').toString() + '\n');
 
-const oldLog = console.log;
-console.log = (msg, primary = true) => {
-	fs.appendFileSync(
-		`./logs/${new Date().toLocaleDateString().replace(/\//g, '.')}.log`,
-		`[Nicory][LOG][${new Date().toLocaleString()}] ${msg}\n`,
-	);
-	if (primary) {
-		oldLog(
-			`[${chalk.magenta('Nicory')}][${chalk.yellowBright(
-				'LOG',
-			)}][${new Date().toLocaleString()}] ${msg}`,
-		);
-	}
-};
+require("./utils/logging")();
 
 const client = new Discord.Client({ fetchAllMembers: true });
 client.commands = new Discord.Collection();
 client.modules = {};
 
-const commandFiles = fs
-	.readdirSync('./commands')
-	.filter((file) => file.endsWith('.js') || file.endsWith('.js'));
-
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	client.commands.set(command.name, command);
-	if (!command.hidden && command.module != undefined) {
-		client.modules[command.module] = client.modules[command.module]
-			? client.modules[command.module]
-			: [];
-		client.modules[command.module].push(command);
-	}
-}
+require("./utils/loadCommands")(client);
 
 client.once('ready', () => {
 	console.log(`Logged in to Discord as ${client.user.tag}`);
---
+++ utils/logging.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const chalk = require("chalk");
+
+module.exports = () => { 
+  const oldLog = console.log;
+  console.log = (msg, primary = true) => {
+    fs.appendFileSync(
+      `./logs/${new Date().toLocaleDateString().replace(/\//g, ".")}.log`,
+      `[Nicory][LOG][${new Date().toLocaleString()}] ${msg}\n`
+    );
+    if (primary) {
+      oldLog(
+        `[${chalk.magenta("Nicory")}][${chalk.yellowBright(
+          "LOG"
+        )}][${new Date().toLocaleString()}] ${msg}`
+      );
+    }
+  };
+};
