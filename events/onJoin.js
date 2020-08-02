c3d7465e07e382ad3d23d6e53d1555a5a27dbc16
NeloExt3 <jabkakek3@gmail.com>
2020-08-02 21:08:18 +0300

    Фиксы в евенте на заход бота на сервер

--- events/onJoin.js
+++ events/onJoin.js
@@ -3,7 +3,8 @@ const { prefix } = require('../config.json');
 
 module.exports = client => {
 	client.on('guildCreate', guild => {
-		const embed = new Discord.MessageEmbed()
+		// сообщение автору гильдии
+		const DMembed = new Discord.MessageEmbed()
 			.setTitle('Nicory Bot')
 			.setDescription(`Доброго дня!\nВы получили это сообщение так как вы являетесь создателем сервера **${guild.name}**!\nЭто чисто информативное сообщение, чтоб вы лучше понимали кто такая Nicory и что она может.`)
 			.setColor('#e155ff')
@@ -20,11 +21,10 @@ module.exports = client => {
 
 				{
 					name: 'Остальное',
-					value: `Стандартный префикс: \`${prefix}\`\nСоздатели:\nNeloExt3#3100\nKislBall#9017\n<:discord:734506565201035264> Discord сервер бота: https://discord.gg/GND9y4e`,
+					value: `Стандартный префикс: \`${prefix}\`\nСоздатели:\nEnotKEK3#3100\nKislBall#9017\n<:discord:734506565201035264> Discord сервер бота: https://discord.gg/GND9y4e`,
 				},
 			)
 			.setFooter('Спасибо что пользуетесь Nicory Bot!', 'https://cdn.discordapp.com/avatars/706600733931339806/b414b516bcf79c9e03a74e10cbd0ceb2.webp');
-		guild.owner.send(embed);
-
+		guild.owner.send(DMembed);
 	});
 };
