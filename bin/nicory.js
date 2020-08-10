62ed001dd317413ca00dd23f2f4e12bfb76a0cc2
kislball <kislball@yahoo.com>
2020-08-11 00:22:14 +0300

    nicory cli

---
+++ bin/nicory.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+const child_process = require("child_process");
+const fs = require("fs");
+
+require("yargs")
+  .command("start", "запуск бота", () => {}, () => {
+    console.log("Запуск Nicory...");
+    child_process.execSync("npm run production");
+  })
+  .command("stop", "остановка бота", () => {}, () => {
+    console.log("Остановка Nicory...");
+    child_process.execSync("npm run production:stop");
+  })
+  .demandCommand(1, '')
+  .epilog("Сделано KislBall и EnotKEK3 💜")
+  .strict()
+  .argv
