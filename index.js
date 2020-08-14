f3a23fd2ea64ec000fbc248298d33e9b295a6b84
Кислый <kislball@yahoo.com>
2020-08-14 12:20:04 +0300

    sharding and other stuff

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const start = new Date();
 const fs = require("fs");
 const Discord = require("discord.js");
-const { token } = require("./config.json");
+const { token, presence } = require("./config.json");
 const processCommand = require("./utils/processCommand");
 const loadProps = require("./utils/loadProps");
 
@@ -9,7 +9,14 @@ console.log(fs.readFileSync("./assets/banner.txt").toString() + "\n");
 
 require("./utils/logging")();
 
-const client = new Discord.Client({ fetchAllMembers: true });
+const client = new Discord.Client({ 
+  fetchAllMembers: true,
+  messageCacheMaxSize: 1000,
+  messageCacheLifetime: 30,
+  messageSweepInterval: 60,
+  presence,
+  shardCount: 1
+});
 client.commands = new Discord.Collection();
 client.modules = {};
 
@@ -18,14 +25,19 @@ client.props = loadProps();
 require("./utils/loadCommands")(client);
 
 client.once("ready", () => {
-  console.log(`Logged in to Discord as ${client.user.tag}`);
+  console.log(`logged in to Discord as ${client.user.tag}`);
 });
+
+client.on("shardReady", shard => console.log(`shard ${shard} is ready`));
+client.on("shardError", (err, shard) => console.log(`shard ${shard} has thrown an error: ${err}`));
+client.on("shardReconnecting", (shard) => console.log(`shard ${shard} is back`));
+client.on("shardDisconnect", (event, shard) => console.log(`shard ${shard} closed with ${event} event`));
 
 client.on("message", async (message) => {
   await processCommand(message, client);
 });
 
-client.on("messageUpdate", async (oldMsg, newMsg) => {
+client.on("messageUpdate", async (_, newMsg) => {
   await processCommand(newMsg, client);
 });
 
